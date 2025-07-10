c1971832e5cbd563c06b485cbcea53956574dcce
Rahul Chatterjee <officialwithrahul@gmail.com>
2025-07-10 14:56:10 +0000

    new push

---
+++ Basics/comparisionofdatatype.js
@@ -0,0 +1,1 @@
+//comparisionofdatatype.js
--- Basics/operations.js
+++ Basics/operations.js
@@ -1,1 +1,46 @@
-console.log("Coding")
+let value=100
+console.log(-value)
+
+//Basic math operations...
+console.log(15+10)
+console.log(15-10)
+console.log(15*10)
+console.log(10**2)
+console.log(15/10)
+console.log(15%10)
+
+//string operations...
+let str1="Hello"
+let str2=" Coders!"
+console.log(str1+str2) // Hello Coders!
+
+console.log("1"+2) // 12
+console.log(1+"2") // 12
+console.log("1"+"2"+3+4) // 1234
+console.log(1+2+3+"4"+"5") // 645
+
+
+console.log(true)
+console.log(false)
+
+console.log(2+true) // 2+1=3
+console.log(2+false) // 2+0=2
+
+// console.log(true+5)
+// console.log(false+9)
+
+
+let num1=num2=num3=4 
+
+// instead of 
+// let num1, num2, num3
+// num1=num2=num3=4
+
+
+let flag=99
+console.log(++flag) //100
+console.log(flag) //100
+
+let count=99
+console.log(count++) //99
+console.log(count) //100
