15a61566aab3a2eaea790481a5272c9c55cd5c68
Rahul Chatterjee <officialwithrahul@gmail.com>
2025-07-10 21:24:05 +0000

    new push

--- Basics/comparisionofdatatype.js
+++ Basics/comparisionofdatatype.js
@@ -1,1 +1,39 @@
-//comparisionofdatatype.js
+//comparision of datatype
+
+console.log(10<20)
+console.log(10>20)
+console.log(10==20)
+console.log(10!=20)
+console.log(10<=30)
+console.log(10>=30)
+
+console.log()
+
+// This is not expected to put numbers as string... Though numerical strings are converted into number  
+console.log("2">1)
+console.log(2<"1")
+console.log("1"=="2")
+
+
+console.log()
+
+console.log(null==0)
+console.log(null>=0) // true
+console.log(null<0)
+
+console.log()
+
+// In undefined all are false
+console.log(undefined==0)
+console.log(undefined<0)
+console.log(undefined>0)
+
+console.log()  
+console.log(0==false)
+console.log(1==true) 
+
+
+// === --> strictly check the datatype...
+
+console.log()
+console.log("2"===2)
