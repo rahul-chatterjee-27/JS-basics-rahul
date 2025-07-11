7f554feaa0620e51787f681307a7ab4d65f6c964
Rahul Chatterjee <officialwithrahul@gmail.com>
2025-07-11 08:22:06 +0000

    New push

--- Basics/memory.js
+++ Basics/memory.js
@@ -1,3 +1,26 @@
 // Stack(Primitive) and Heap(non-primitive)...
 
-let Offical_name="Rahul Chatterjee"
+let Offical_name="Indranil Sanyal"
+
+let pen_name=Offical_name
+pen_name="Bidhu"
+
+
+console.log(Offical_name)
+console.log(pen_name)
+
+
+
+
+
+let user_1={
+    Name:"Rahul Chatterjee",
+    Email_ID:"user1email@google.com",
+    Upi_ID:"123456@ybl"
+}
+
+let user_2=user_1
+user_2.Email_ID="user2email@google.com"
+
+console.log(user_1.Email_ID)
+console.log(user_2.Email_ID)
---
+++ Basics/string.js
@@ -0,0 +1,13 @@
+const Name="Rahul"
+console.log("Name:",Name)
+
+const Surname=' Chatterjee'
+console.log("Surname:",Surname)
+
+const Full_name=Name+Surname
+console.log("Full name:",Full_name)
+
+
+const name="rahul"
+console.log(name+"-chaterjee-"+27)
+
