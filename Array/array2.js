330213ebfc526c61cc489a613d9c6797995e70ad
Rahul Chatterjee <officialwithrahul@gmail.com>
2025-07-12 19:53:39 +0000

    new push

--- Array/array2.js
+++ Array/array2.js
@@ -21,6 +21,15 @@ console.log(new_array2);
 
 console.log(Array.isArray("Developer"));
 console.log(Array.from("Developer"));
+console.log(Array.from({Name: "JavaScriprt"}));
+
+console.log();
+let num1=100;
+let num2=200;
+let num3=300;
+
+console.log(Array.of(num1,num2,num3));
+
 
 
 
