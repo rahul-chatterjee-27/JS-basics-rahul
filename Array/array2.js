715c44e49b86306e7ec6531055fda197c7b616d6
Rahul Chatterjee <officialwithrahul@gmail.com>
2025-07-12 15:45:33 +0000

    new push

--- Array/array2.js
+++ Array/array2.js
@@ -6,3 +6,29 @@ console.log(song);
 
 book.push(song) //Array under array MEANS ARRAY AS AN ELEMENT... 
 console.log(book)
+console.log(book[3][1])
+
+const merge=book.concat(song) //Merge without being an array...
+console.log(merge)
+
+const merge2=[...book,...song] //Merge by spread method...
+console.log(merge)
+
+console.log()
+const new_array=[1,2,3,4,[5,6,7],8,9,[10,11,[12,[13]]]]
+const new_array2=new_array.flat(Infinity) //Make it in a single array...
+console.log(new_array2);
+
+console.log(Array.isArray("Developer"));
+console.log(Array.from("Developer"));
+
+
+
+
+
+
+
+
+
+
+
