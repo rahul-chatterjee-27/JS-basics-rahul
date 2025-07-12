416ed24f9e3eee99ed6aa735a9bcc4c6be22868c
Rahul Chatterjee <officialwithrahul@gmail.com>
2025-07-12 07:34:58 +0000

    new push

---
+++ Array/basicsarray.js
@@ -0,0 +1,15 @@
+const array=[1,56,"Rahul",true,45.7,"Developer"] //Contain different types of datatypes
+console.log(array);
+
+const myarray=[21,66,8,9,2,3,16,10]
+const myarray2=new Array(1,2,3,4,5,6,7,8,9,10) //No need to give []
+console.log(myarray);
+console.log(myarray2);
+console.log(myarray2[3]); // 0 based indexing...
+
+const book=["Thriller","Romance","Suspence","Comedy","Horror","Sci-fi","Detective"]
+console.log(book)
+console.log(book.length)
+
+
+
