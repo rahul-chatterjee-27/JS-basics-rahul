a99180b8c5eda51a6c45bd3c470062abf817d7dd
Rahul Chatterjee <officialwithrahul@gmail.com>
2025-07-13 07:34:20 +0000

    new push

--- Object/object2.js
+++ Object/object2.js
@@ -1,6 +1,48 @@
-const gituser=new Object() //Singletone Object
+const gituser=new Object() //Singletone Object -->  one shared object,
 console.log(gituser);// {}
 
-const githubuser={}// Non-singletone object
+const githubuser={}// Non-singletone object --> many separate objects
 console.log(githubuser); // {}
+
+const fbuser={
+    id: "fbrahul2468",
+    name: "Rahul",
+    email: "rahul@facebook.com",
+    isLoggedIn: false
+}
+console.log(fbuser);
+
+const instauser={
+    email:"rahul2468@instagram.com",
+    details: {
+        fullname:{
+            firstname: "Rahul",
+            surname: "chatterjee"
+        },
+        instaid:"123rc456",
+    },
+    isLoggedIn: false
+}
+console.log(instauser);
+console.log(instauser.details);
+console.log(instauser.details.fullname);
+console.log(instauser.details.fullname.firstname);
+
+
+console.log();
+console.log();
+
+const obj1={1:"A",2:"B"}
+console.log(obj1);
+const obj2={3:"C",4:"D"}
+console.log(obj2);
+
+const obj3={obj1,obj2}
+console.log(obj3); //{ obj1: { '1': 'A', '2': 'B' }, obj2: { '3': 'C', '4': 'D' } }
+
+const obj4=Object.assign(obj1,obj2)
+console.log(obj4); //{ '1': 'A', '2': 'B', '3': 'C', '4': 'D' }
+
+
+
 
