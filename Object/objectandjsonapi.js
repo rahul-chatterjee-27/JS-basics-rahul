44d1f8e755a9db6f85ae1af0555aabf107a73db5
Rahul Chatterjee <officialwithrahul@gmail.com>
2025-07-14 07:59:55 +0000

    new push

--- Object/objectandjsonapi.js
+++ Object/objectandjsonapi.js
@@ -1,4 +1,18 @@
-//Object destructuring & JSON API 
+//Object destructuring & JSON API   //node Object/objectandjsonapi.js
 
 
-//node Object/objectandjsonapi.js
+
+const course={
+    name: "JS Basics",
+    price: 699,
+    instructor: "Rahul",
+    duration: "one month"
+}
+
+//onsole.log(course.instructor); //Not a preferred method...
+const {instructor}=course
+console.log(instructor);
+
+const {instructor: inst}=course //We can rename the variable too... (like instructur to inst)
+console.log(inst);
+
