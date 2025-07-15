7a69e4b13d1cb848244a64c3ef1877dbfd0fedcb
Rahul Chatterjee <officialwithrahul@gmail.com>
2025-07-15 13:28:57 +0000

    new push

--- Scope/globalandlocalscope.js
+++ Scope/globalandlocalscope.js
@@ -1,1 +1,29 @@
-// node Scope/globalandlocalscope.js
+// let a=10
+// const b=20
+// var c=30
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+{} //scope 
+
+if(true){
+    let a1=10
+    const b1=20
+    var c1=30
+}
+
+//console.log(a1); //a1 is not defined
+//console.log(b1); //b1 is not defined
+//console.log(c1); //30 
+
+
+//Outside of {...} --> Global Scope
+//Inside of {...} --> Local Scope
+
+let num_1=100
+if(true){
+    let num_1=20
+    console.log("Local Scope Value: ",num_1)
+}
+console.log("Global Scope Value: ",num_1)
