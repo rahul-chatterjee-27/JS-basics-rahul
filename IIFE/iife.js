a36c08d6d8c0cea195e8e90ef54fa1ef8e302e9a
Rahul Chatterjee <officialwithrahul@gmail.com>
2025-07-15 16:24:20 +0000

    new push

--- IIFE/iife.js
+++ IIFE/iife.js
@@ -1,1 +1,13 @@
-//Immediately Invoked Function Expression
+//Immediately Invoked Function Expression...
+
+// //Method-1
+// function code(){
+//     console.log('Database Connected');
+// }
+// code()
+
+//Method-2
+(function coding(){
+    console.log('Database Connected');
+})() 
+
